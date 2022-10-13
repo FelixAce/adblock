023473d959e69b467bbdf11fb88eba1d9456542a
FelixAce <85485765+FelixAce@users.noreply.github.com>
2022-10-13 19:56:58 +0800

    Update surge_javdb.js

--- adguard/surge_javdb.js
+++ adguard/surge_javdb.js
@@ -1,5 +1,5 @@
 var Oldone = '<head>'
-var Newone = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/FelixAce/adblock/main/css/javdb.css?token=GHSAT0AAAAAABXWLBLGRIH7MJHMXIKIHQ6OY2H63JQ" type="text/css"><script type="text/javascript"  src="//raw.githubusercontent.com/FelixAce/adblock/main/adguard/surge_javdb.js?token=GHSAT0AAAAAABXWLBLHGSOY24EWJAF2KSY4Y2H65EA"></script>';
+var Newone = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/FelixAce/adblock/main/css/javdb.css" type="text/css"><script type="text/javascript"  src="//raw.githubusercontent.com/FelixAce/adblock/main/adguard/surge_javdb.js"></script>';
 let body = $response.body
 .replace(Oldone, Newone);
 $done({body});
