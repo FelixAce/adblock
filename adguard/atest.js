a2281bbff36f892e5ba1c8f94cd9808aef463b67
FelixAce <85485765+FelixAce@users.noreply.github.com>
2022-10-14 09:57:05 +0800

    Update atest.js

--- adguard/atest.js
+++ adguard/atest.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/FelixAce/adblock/main/css/javdb.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://wsx.felix188.top/adblock/css/javdb.css" type="text/css">');
 $done({ body });
