c562905303f35faa87ea62d7e1736caa8ea79f03
FelixAce <85485765+FelixAce@users.noreply.github.com>
2022-10-14 09:00:27 +0800

    Update test.js

--- adguard/test.js
+++ adguard/test.js
@@ -1,5 +1,5 @@
-function() {
-    var ads = document.querySelectorAll('.moj-content,.app-desktop-banner,.sub-header')
+function javdb_adsRemove{
+    var ads = document.querySelectorAll('.moj-content,.app-desktop-banner,.sub-header');
         for (var i = 0; i < ads.length; i++) {
             var ad = ads[i];
             ad.style.display = "none";
