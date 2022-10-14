7314ce5c05bb59611cdf8087f1b6b459b8d9d6b2
FelixAce <85485765+FelixAce@users.noreply.github.com>
2022-10-14 13:48:14 +0800

    Delete surge_javdb.js

--- adguard/surge_javdb.js
+++
@@ -1,5 +0,0 @@
-var Oldone = '<head>'
-var Newone = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/FelixAce/adblock/main/css/javdb.css" type="text/css">;
-let body = $response.body
-.replace(Oldone, Newone);
-$done({body});
