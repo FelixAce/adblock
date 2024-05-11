85a54c01a05b5e8e4223106f8cb6aa0265fba339
FelixAce <85485765+FelixAce@users.noreply.github.com>
2024-05-11 15:46:28 +0800

    Delete adguard directory

--- adguard/javlibrary.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://wb.fireflyos.xyz/adblock/css/javlibrary.css" type="text/css">');
-$done({ body });
--- adguard/sehuatang.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://wb.fireflyos.xyz/adblock/css/sehuatang.css" type="text/css">');
-$done({ body });
